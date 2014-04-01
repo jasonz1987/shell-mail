152fe95ff5a2e1895f964307079a01eca09f3ad1
jasonz1987 <281674669@qq.com>
2014-04-01 14:38:32 +0800

    1.2
    add file check

--- mail.sh
+++ mail.sh
@@ -7,14 +7,18 @@
 
 
 from="sender@example.com"
+#you can give more receiver like a@example.com b@example.com ...
 to="reciever@example.com"
 subject="Your email title"
+#you can also read content from the file just use $(cat yourfile)
 body="This is body content of your email"
+
 declare -a attachments
 attachments=( "a.pdf" "b.zip" )
 
 declare -a attargs
 for att in "${attachments[@]}"; do
+   [ ! -f "$att" ] && echo "Warning: attachment $att not found, skipping" >&2 && continue	
   attargs+=( "-a"  "$att" )
 done
 
