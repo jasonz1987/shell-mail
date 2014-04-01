db8e73e46d115b190949fd9ec570d49af0fdf478
jasonz1987 <281674669@qq.com>
2014-04-01 14:53:56 +0800

    1.3
    mv mail-stmp mail-smtp

--- mail-smtp.sh
+++ mail-smtp.sh
@@ -5,19 +5,30 @@
 # Mail:ccnuzxg@gmail.com
 # Date:2014-04-01
 
-
+# must be same with STMP account addr
 from="sender@example.com"
+# more receiver like a@example.com b@example.com ...
 to="reciever@example.com"
 subject="Your email title"
+#you can also read content from the file just use $(cat yourfile)
 body="This is body content of your email"
+
 declare -a attachments
 attachments=( "a.pdf" "b.zip" )
 
+#deal with attachment args
+declare -a attargs
+for att in "${attachments[@]}"; do
+   [ ! -f "$att" ] && echo "Warning: attachment $att not found, skipping" >&2 && continue	
+  attargs+=( "-a"  "$att" )
+done
 
-smtpserver="10.10.10.10"
-smtpport="443"
-$user="test"
-$password="123456"
+# smtp server info
+smtpserver="smtp.example.com"
+smtpport="25"
+user="sender"
+password="123456"
+
 
 mail -s "$subject" -r "$from" -S smtp="smtp://${smtpserver}:${smtpport}" \
                               -S smtp-auth=login \
--- mail.sh
+++ mail.sh
@@ -7,7 +7,7 @@
 
 
 from="sender@example.com"
-#you can give more receiver like a@example.com b@example.com ...
+# more receiver like a@example.com b@example.com ...
 to="reciever@example.com"
 subject="Your email title"
 #you can also read content from the file just use $(cat yourfile)
@@ -16,6 +16,7 @@ body="This is body content of your email"
 declare -a attachments
 attachments=( "a.pdf" "b.zip" )
 
+#deal with attachment args
 declare -a attargs
 for att in "${attachments[@]}"; do
    [ ! -f "$att" ] && echo "Warning: attachment $att not found, skipping" >&2 && continue	
